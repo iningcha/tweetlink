97b7bbac58124518050ea5a1e5343d2fb5837889
Samantha I-Ning Chang <iningcha@usc.edu>
2020-05-20 18:18:21 -0700

    add express server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+
+  useEffect(() => {
+    callBackendAPI()
+      .then(res => setData(res.express))
+      .catch(err => console.log(err));
+  });
+
+  const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +38,11 @@ function App() {
         >
           Learn React
         </a>
+        <p className="App-intro">{data}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
